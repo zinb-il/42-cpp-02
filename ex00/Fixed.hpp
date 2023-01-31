a3b103d9c0aeb99e8ef25d1971aa076e4d824771
zinb9419 <zinb9419@gmail.com>
2023-01-31 17:44:44 +0100

    c02

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ziloughm <ziloughm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 12:54:44 by ziloughm          #+#    #+#             */
+/*   Updated: 2023/01/31 17:42:36 by ziloughm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#	ifndef FIXED_HPP
+# define FIXED_HPP
+
+class Fixed
+{
+    private:
+        int _fixed_nb;
+        static int const _frac_bit = 8;
+    public:
+        Fixed();
+        ~Fixed();
+        Fixed(int n);
+        Fixed(Fixed const &ob);
+        Fixed & operator=(Fixed const &ob);
+        
+        
+        int getRawBits(void) const;
+        void setRawBits( int const raw );
+};
+
+std::ostream & operator<<(std::ostream & o, Fixed const & ref);
+
+#	endif
---
+++ ex00/main.cpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ziloughm <ziloughm@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/31 17:12:27 by ziloughm          #+#    #+#             */
+/*   Updated: 2023/01/31 17:43:30 by ziloughm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.h"
+
+int main( void ) {
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
